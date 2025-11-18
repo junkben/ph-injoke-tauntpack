d1ca0f2d47005122967c8f673cc34fdad674143a
Ben Harruff <harruff@pm.me>
2025-11-18 16:12:49 -0500

    alright sick

---
+++ garrysmod/lua/autorun/sh_taunts.lua
@@ -0,0 +1,29 @@
+-- https://github.com/Wolvin-NET/prophuntx/blob/master/templates/lua/autorun/sh_additional_taunts.lua
+
+local TEAM_HUNTERS = 1
+local DIR_HUNTERS  = "bumblezone_hunters/"
+
+local TEAM_PROPS   = 2
+local DIR_PROPS    = "bumblezone_props/"
+
+local FILE_EXT     = ".ogg"
+
+local taunts       = {
+    [TEAM_HUNTERS] = {},
+    [TEAM_PROPS] = {
+        ["Jesus Is the Bread 3"]                        = "jesus-is-the-bread",
+        ["Lil Kiddo Fucking Dies"]                      = "lil-kiddo-fucking-dies",
+        ["Spiderman 3 Fart Jump With Scooby Doo Laugh"] = "spiderman-3-fart-jump-with-scooby-doo-laugh",
+        ["Turn That Poop Into Wine"]                    = "turn-that-poop-into-wine",
+        ["Watch Your Profanity"]                        = "watch-your-profanity"
+    }
+}
+
+for key, value in pairs(taunts[TEAM_HUNTERS]) do
+    taunts[TEAM_HUNTERS][key] = DIR_HUNTERS .. value .. FILE_EXT
+end
+for key, value in pairs(taunts[TEAM_PROPS]) do
+    taunts[TEAM_PROPS][key] = DIR_PROPS .. value .. FILE_EXT
+end
+
+list.Set("PHX.CustomTaunts", "The Bumblezone", taunts)
